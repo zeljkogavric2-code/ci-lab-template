f2cfe2282347856c5a2f60426619388cc731bae4
admin <admin@HOST>
2025-12-23 23:39:26 +0100

    Add CalculatorService and basic tests

---
+++ Calculator.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Calculator.Tests;
+
+public class CalculatorServiceTests
+{
+    private readonly CalculatorService _calculator = new();
+
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var result = _calculator.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        var result = _calculator.Subtract(5, 3);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsException()
+    {
+        Assert.Throws<DivideByZeroException>(() =>
+            _calculator.Divide(10, 0));
+    }
+}
---
+++ Calculator/CalculatorService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b; // namerno bez provere nule
+        }
+    }
+}
